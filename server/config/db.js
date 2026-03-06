eab4292d3adb447b58ebb786f74101a091799d2d
udayapex1 <udaypareta645@gmail.com>
2026-03-06 22:01:09 +0530

    Production Setup

---
+++ server/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export const ConnectDB = async () =>{
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    mongoose.connection.on("disconnected", () => {
+      console.log("MongoDB Disconnected");
+    });
+
+    mongoose.connection.on("reconnected", () => {
+      console.log("MongoDB Reconnected");
+    });
+
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+}
--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 import express from "express";
 import dotenv from "dotenv"
- 
+import { ConnectDB } from "./config/db.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
+ConnectDB();
 
+app.get("/health", (req, res) => {
+  const healthcheck = {
+    status: "OK",
+    service: "pnr-status-api",
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+    database:
+      mongoose.connection.readyState === 1 ? "connected" : "disconnected"
+  };
 
-app.use("/testing", (req, res) => {
-    res.send("<h1>Server Instance is running<h1/> ")
-})
+  res.status(200).json(healthcheck);
+});
 
 const port = process.env.PORT;
 
