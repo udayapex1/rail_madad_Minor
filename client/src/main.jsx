fb2edbd6b1c8ef83c1cda4f189dcb2dd3481b51b
udayapex1 <183940025+udayapex1@users.noreply.github.com>
2026-03-31 23:28:45 +0530

    fix bug during merge

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-);
-=======
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -20,4 +9,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 )
->>>>>>> dev
